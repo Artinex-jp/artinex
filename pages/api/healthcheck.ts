3ead0363d859232f77cc62567194cdd603017f24
Masanori Enoki <info@artinex.jp>
2025-06-18 22:10:13 +0900

    supabaseとの連携チェック

---
+++ pages/api/healthcheck.ts
@@ -0,0 +1,15 @@
+import { createClient } from "@supabase/supabase-js";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const supabase = createClient (
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	const {data, error} = await supabase.from("customers").select("*").limit(1);
+	if (error) {
+		return res.status(500).json({error: error.message})
+	}
+	res.status(200).json({success: true, data})
+}
