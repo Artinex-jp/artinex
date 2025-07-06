194c75544bbf94f0ef6d44dee805d51e8981c51c
Masanori Enoki <info@artinex.jp>
2025-07-07 00:25:33 +0900

    デバッグ用データを消去

--- pages/api/debug-env.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.json({
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    key: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  })
-}
