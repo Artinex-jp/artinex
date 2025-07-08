a2d4735a47d9b9cc66a733cf1f0733e9b8562034
MasarnoriEnokiPhonim <enoki@phonim.co.jp>
2025-07-08 17:31:18 +0900

    会社概要ページを作成

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,15 +4,18 @@ export default function Footer() {
   return (
     <footer className="bg-gray-100 text-gray-700 text-sm border-t">
       <div className="max-w-xl mx-auto px-4 py-6 md:flex-row justify-between items-center gap-4">
-        <div className="flex flex-wrap space-x-6 gap-3 mb-4">
+        <div className="flex flex-col md:flex-row md:justify-between items-center gap-3 mb-4">
           <Link href="/" className="text-gray-500 font-bold hover:text-gray-700 no-underline">
             HOME
           </Link>
           <Link href="/events" className="text-gray-500 font-bold hover:text-gray-700 no-underline">
             イベント一覧
           </Link>
-          <Link href="https://www.artinex.jp/tokushoho" className="text-gray-500 font-bold hover:text-gray-700 no-underline">
+          <Link href="/tokushoho" className="text-gray-500 font-bold hover:text-gray-700 no-underline">
             特定商取引法に基づく表示
+          </Link>
+          <Link href="/company" className="text-gray-500 font-bold hover:text-gray-700 no-underline">
+            会社概要
           </Link>
           <Link href="/#contact" className="text-gray-500 font-bold hover:text-gray-700 no-underline">
             お問い合わせ
